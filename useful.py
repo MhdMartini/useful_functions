003b9e0b90522e70c35bc024fe79d35bf32b5e86
Mohamed Martini <mohamed_martini@student.uml.edu>
2020-06-12 10:48:46 -0400

    This script contains functions I have written which I find useful.
    Currently including:
    1- Multi-key dictionary class
    
    2- Function which persistently prompts for user input until a valid input is received

---
+++ useful.py
@@ -0,0 +1,41 @@
+import sys
+
+# Persistently prompt the user until user input is in "lst"
+def p_prompt(prompt, lst):
+    while True:
+        ans = input(prompt)
+        if ans not in lst:
+            print('Invalid choice!')
+            print()
+            continue
+        return ans
+
+    
+class DictPlus():
+    def __init__(self, dictionary):
+        if type(dictionary) == dict:
+            self.dictionary = dictionary
+        else:
+            self.dictionary == dict(dictionary)
+
+    def get(self, ke):
+        self.dictionary = self.dict_expand()
+        for key in self.dictionary:
+            if (ke == key) or (ke in key):
+                return self.dictionary[key]
+
+
+    def help(self):
+        help_msg = '''
+Use the DictPlus class to give your dictionary the ability to return a dictionary value from than one dictionary key.
+
+* Syntax:
+    dict_plus = DictPlus(arg)
+        - 'arg' could either be a type dict variable, or a sequence such that type( dict(sequence) ) == dict 
+
+* Methods:
+    - DictPlus.get(key)
+        returns the dictionary value if 'key' is among the dictionary keys, or in a sequence within a key
+        
+'''
+
